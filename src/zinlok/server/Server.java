af0de11486aff9082ad20e7e2e6c2f5d8cddedad
LoloCDF <manaraani@alum.us.es>
2016-04-20 00:20:01 +0200

    En la clase principal (Server) se ha conseguido con éxito montar un servidor.

--- src/zinlok/server/Server.java
+++ src/zinlok/server/Server.java
@@ -1,7 +1,27 @@
 package zinlok.server;
 
+import java.net.*;
+
 public class Server {
 	public static void main (String[] args){
-		System.out.println("Hola");
+		int puerto=5000;
+		
+		ServerSocket skServidor;
+		
+		Socket skCliente;
+		
+		try{
+			skServidor = new ServerSocket(puerto);
+			System.out.println("Escucho el puerto"+puerto);
+			
+			while(true){
+				skCliente=skServidor.accept();
+				System.out.println("Sirvo al cliente: " + skCliente.getInetAddress().toString());
+				skCliente.close();
+			}
+		} catch( Exception e){
+			System.out.println(e.getMessage());
+		}
+	
 	}
 }
